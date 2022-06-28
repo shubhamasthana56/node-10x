6894e4051499c387d74511ab1c273956c19057d3
shubham asthana <shubhamasthana56@gmail.com>
2022-06-28 19:38:53 +0530

    http and file reading changes

--- express/server.js
+++ express/server.js
@@ -1,23 +1,23 @@
 const express = require("express");
+const path = require("path");
+const http = require("http");
+//__dirname __file
 const app = express();
 const userController = require("./user");
 app.listen(3001);
+// http.createServer((req, res)=> {
+//     res.end()
+// }).listen(5000)
+const text = "Shubham";
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.get("/",(req, res)=> {
-    console.log(req.query);
-    const userData = [{
-        firstName: "Shubham",
-        age: 23
-    },
-    {firstName: "shubham1", age:24}
-]
-    const filteredUserData = userData.filter((user)=> {
-        return user.firstName.includes(req.query.name)
-    })
-    console.log(filteredUserData)
-    res.send(filteredUserData);
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+app.get("/about",(req, res)=> {
+    res.sendFile(path.join(__dirname + '/about.html'))
 });
 
 
