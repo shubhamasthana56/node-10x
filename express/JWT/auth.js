75d7d1dd4ebb82dce29383cea34012835e84e305
shubham asthana <shubhamasthana56@gmail.com>
2022-07-03 12:54:48 +0530

    jwt addition

--- express/JWT/auth.js
+++ express/JWT/auth.js
@@ -2,13 +2,23 @@ const express = require("express");
 const router = express.Router();
 const {userInfo} = require("../common/modal/user-modal");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
+
+//Get secretkey 64 hexadecimal string 
+//payload
+const generateSecretKey = ()=> {
+    return crypto.randomBytes(64).toString("hex");
+}
 router.post("/login", (req, res)=> {
     userInfo.find({username: req.body.username}).then((user)=> {
         if(user.length) {
             bcrypt.compare(req.body.password,user[0].password).then((val)=> {
                 if(val) {
-                    res.status(200).send("User Loggedin Successfully")
+                    const secret = generateSecretKey();
+                    const authToken = jwt.sign(user[0].username, secret);
+                    res.status(200).send({authToken});
                 } else {
                     res.status(401).send("Invalid Password")
                 }
